03394b9d368dca0fa5c9a8ab13c6e7f28510637b
Furkan <danismaz.furkan@gmail.com>
2018-09-13 18:21:35 +0300

    headers removed

--- jpa-specification/src/main/java/com/fd/jpa/controller/PersonController.java
+++ jpa-specification/src/main/java/com/fd/jpa/controller/PersonController.java
@@ -1,15 +1,3 @@
-/*
- * Telif hakkı 2017 Türkiye Bilimsel ve Teknolojik Araştırma Kurumu.
- * Tüm haklar saklıdır.
- *
- * BU TELİF HAKKI DOSYA ÜST BİLGİSİNİ DEĞİŞTİRMEYİNİZ VEYA SİLMEYİNİZ.
- *
- * Bu kaynak kod sadece TÜBİTAK için TYBS Projesi kapsamında geliştirilmiştir;
- * değişiklik yapma, başka amaçlarda kullanılmak üzere kısmen veya tamamını
- * kopyalama ve/veya dağıtım yapma hakkınız bulunmamaktadır.
- *
- * Daha fazla bilgi ve sorularınız için TÜBİTAK ile iletişime geçiniz.
- */
 package com.fd.jpa.controller;
 
 import com.fd.jpa.converter.CycleAvoidingMappingContext;
