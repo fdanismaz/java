b127bcf0d71c944c99fc5ca1076ed6bb11744d69
fdanismaz <danismaz.furkan@gmail.com>
2018-10-05 07:26:26 +0300

    * Spring boot application is created * Spring Session configuration is made with REDIS

---
+++ security-jwt/stateless-auth/src/main/java/com/fd/tryout/jwt/App.java
@@ -0,0 +1,17 @@
+package com.fd.tryout.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fdanismaz
+ * date: 10/5/18 6:05 AM
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
---
+++ security-jwt/stateless-auth/src/main/java/com/fd/tryout/jwt/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.fd.tryout.jwt.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author fdanismaz
+ * date: 10/5/18 6:20 AM
+ */
+@Configuration
+public class SecurityConfig {
+}
